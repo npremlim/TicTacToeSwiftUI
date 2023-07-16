3b16f4c09e11cba597d42dc0056f8dae402090c1
Naomi <naomipremlim@gmail.com>
2023-07-15 19:30:25 -0700

    Initial Commit

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Naomi on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
